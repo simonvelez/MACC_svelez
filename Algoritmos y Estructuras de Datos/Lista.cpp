866ab537c05998525539111d10c32222918fe446
Simón Vélez <145301431+simonvelez@users.noreply.github.com>
2023-09-25 10:28:33 -0500

    Add files via upload

---
+++ Algoritmos y Estructuras de Datos/Lista.cpp
@@ -0,0 +1,144 @@
+#include<iostream>
+
+using namespace std;
+
+template<typename T>
+class Nodo{
+private:
+  T dato;
+  Nodo* next;
+
+public:
+
+ Nodo(T d){
+     dato = d;
+     next =  NULL;
+ }
+ 
+ ~Nodo(){
+ }
+ 
+ T get_dato(){
+     return dato;
+ }
+ 
+ void set_dato(T d){
+     dato = d;
+ }
+ 
+ Nodo* get_next(){
+     return next;
+ }
+ 
+ void set_next(Nodo* n){
+     next = n;
+ }
+ 
+    
+};
+
+template<typename T>
+class Lista{
+private:
+  Nodo<T>* ptr;
+  int size;
+
+public:
+
+    Lista(){
+        ptr = NULL;
+        size = 0;
+    }
+  
+    ~Lista(){
+        Nodo<T>* temp = ptr;
+        if(ptr != NULL){
+            Nodo<T>* temp_next = ptr->get_next();
+            while(temp_next != NULL){
+                delete temp;
+                temp = temp_next;
+                temp_next = temp->get_next();
+            }
+            delete temp; //Borrar el último nodo
+        }
+    }
+    
+    void add(T d){
+        Nodo<T>* nodo = new Nodo<T>(d);
+        
+        if(ptr == NULL){//La lista está vacía
+            ptr  = nodo;
+        }else{//La lista no está vacía
+            Nodo<T>* temp =  ptr;
+            while(temp->get_next() != NULL){
+                temp = temp->get_next();
+            }
+            temp->set_next(nodo);
+        }
+        size++;
+    }
+    
+    void insert(T d, int i){
+        if(i<=size && i>=0 && ptr != NULL){
+            Nodo<T>* nodo = new Nodo<T>(d);
+            if(i == 0){
+                nodo->set_next(ptr);
+                ptr = nodo;
+            }else{
+                int j = 0;
+                Nodo<T>* temp =  ptr;
+                while(j<i-1){
+                    temp = temp->get_next();
+                    j++;
+                }
+                nodo->set_next(temp->get_next());
+                temp->set_next(nodo);
+            }
+            size++;
+        }else{//Si el índice es incorrecto o la lista está vacía, se añade al final
+            add(d);
+        }
+        
+    }
+    
+    void print(){
+        if(ptr == NULL){//La lista está vacía
+            cout<<"La lista está vacía"<<endl;
+        }else{//La lista no está vacía
+            Nodo<T>* temp =  ptr;
+            while(temp != NULL){
+                //cout<<temp->get_dato()<<","<<temp->get_next()<<"\t";
+                cout<<temp->get_dato()<<"\t";
+                temp = temp->get_next();
+            }
+            cout<<endl;
+        }
+    }
+    
+    Nodo<T>* get(int i){
+        if(i>=0 && i<size){
+          int j=0;
+          Nodo<T>* temp = ptr;
+          while(j<i){
+              temp = temp->get_next();
+              j++;
+          }
+          return temp;
+        }
+        return NULL;
+    }
+    
+};
+
+
+
+int main()
+{
+    Lista<int> l;
+    int n = 40;
+    for(int i=0; i<n; i++){
+        l.add(rand()%n);
+    }l.print();
+    
+    return 0;
+}
