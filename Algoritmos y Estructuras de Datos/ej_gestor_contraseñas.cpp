614ecba45d27e82af3ae255e9808c448a5d9c095
Simón Vélez <145301431+simonvelez@users.noreply.github.com>
2023-10-28 08:21:15 -0500

    Implementación con conjuntos

--- Algoritmos y Estructuras de Datos/ej_gestor_contraseñas.cpp
+++ Algoritmos y Estructuras de Datos/ej_gestor_contraseñas.cpp
@@ -8,6 +8,7 @@ Universidad del Rosario, 2023
 
 #include <iostream>
 #include <map>
+#include <set>
 using namespace std;
 
 void print(map<string, string> m){
@@ -21,10 +22,28 @@ void print(map<string, string> m){
 
 int main()
 {
+    // Sets con los conjuntos para identificar letras y números
+    set<char> numeros;
+    set<char> mayus;
+    set<char> minus;
+    map<string, string> m; //mapa donde se guardan las contraseñas
+    
+    for (int i = 0; i<10; i++ ){  //Agrega números del 1 al 9 a numeros en char.
+        numeros.insert(i + '0');
+    }
+    string temp_min = "abcdefghijklmnñopqrstuvwxyz";
+    for (int i = 0; i<28; i++ ){  //Agrega las letras minúsculas a minus.
+        minus.insert(temp_min[i]);
+    }
+    string temp_may = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
+    for (int i = 0; i<28; i++ ){  //Agrega las letrass mayústuclas a mayus.
+        mayus.insert(temp_may[i]);
+    }
+    
     string temp;
     cout<<"¿Agregar un usuario nuevo? (Y: sí, N: no)"<<endl;
     cin>>temp;
-    map<string, string> m;
+    
     while (temp == "Y" || temp == "y" || temp == "si"){
         
         string usua = "";
@@ -36,23 +55,23 @@ int main()
         
         bool may = false, min = false, num = false, punt = false; // Verificar que tenga mayúsculas, minúsculas, números y carácteres especiales
         for (int i=0; i<contra.length(); i++){
-            if (isdigit(contra[i])){
+            if (numeros.count(contra[i])){
                 num = true;
             }
-            if (isupper(contra[i])){
+            if (mayus.count(contra[i])){
                 may = true;
             }
-            if (islower(contra[i])){
+            if (minus.count(contra[i])){
                 min = true;
             }
-            if (not (islower(contra[i]) or isupper(contra[i]) or isdigit(contra[i]))){ //Si no es num, minúscula o mayúscula, es caracter especial
+            if (not (numeros.count(contra[i]) or mayus.count(contra[i]) or minus.count(contra[i]))){ //Si no es num, minúscula o mayúscula, es caracter especial
                 punt = true;
             }
         }
-        if (may and min and num and punt and contra.length() > 3){
+        if (may and min and num and punt and contra.length() > 4){
             m[usua] = contra;
         }else{
-            cout<<"La contraseña debe incluir por lo menos una mayúscula, una mínuscula, un carácter especial, un número y 4 caracteres.";
+            cout<<"La contraseña debe incluir por lo menos una mayúscula, una mínuscula, un carácter especial, un número y 5 caracteres.";
         }
       print(m);
       cout<<endl<<"- - - - - - - - - - - - - - - - - - -"<<endl;
@@ -61,5 +80,6 @@ int main()
     }
     cout<<"Usuarios y contraseñas finales: "<<endl;
     print(m);
+    
     return 0;
 }
