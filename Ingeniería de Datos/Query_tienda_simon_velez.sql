7428d5b75f12d43ca6f884e6ce4e47406840271c
Simón Vélez <145301431+simonvelez@users.noreply.github.com>
2024-02-09 07:53:19 -0600

    Add files via upload

--- Ingeniería de Datos/Query_tienda_simon_velez.sql
+++ Ingeniería de Datos/Query_tienda_simon_velez.sql
@@ -36,3 +36,14 @@ valorArt numeric(7,2),
 constraint Detalle_pk primary key (id_detalle),
 constraint detalle_numfactura_fk foreign key (detalle_numfactura_fk) references factura ("numFactura")
 );
+
+-- Insertar datos
+insert into clientes (NombreCliente, NumeroDocumento, id_cliente, direccion) 
+Values 
+('Javier Casas', '25648421', 1, 'calle 34 Nr. 25-36'),
+('Martin el pin', '48421256', 2, 'en chiquinquirá'),
+('Alejandro', '84212564', 3, 'onde el loko'),
+('Patas', '26484215', 4, 'sin dirección');
+
+
+select * from clientes order by id_cliente;
