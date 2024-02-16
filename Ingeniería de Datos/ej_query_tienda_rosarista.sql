2b8406cce587a1e78d9cfa5c1db36fa272acc5af
Simón Vélez <145301431+simonvelez@users.noreply.github.com>
2024-02-16 07:35:24 -0600

    Add files via upload

--- Ingeniería de Datos/ej_query_tienda_rosarista.sql
+++ Ingeniería de Datos/ej_query_tienda_rosarista.sql
@@ -1,3 +1,5 @@
+--14/02/23
+
 --listado con un nombre del cliente y artículos relacionados con la fact #10
 
 select numfactura, nombrecliente, nombreart 
@@ -29,3 +31,72 @@ where factura.id_cliente = clientes.id_cliente;
 select numfactura, nombrecliente, fechapag, valorpagado, tipopago
 from factura natural join clientes natural join pagos
 where numfactura =  pagos_num_factura_fk; -- no hay que comparar id_cliente porque se llaman igual
+
+
+-- 16/02/24
+-- Renombrando bainas largas
+
+select C.nombrecliente, F.numfactura
+from clientes as C, factura as F
+where C. id_cliente = F.factura_id_cliente_fk;
+
+-- Nombres que tengan go en alguna parte
+
+select nombrecliente
+from clientes
+where nombrecliente like '%go%';
+
+-- el / hace que salga el %
+insert into clientes
+values(11, 'Jav%as Pachon Hernandez', '123', 'en mi casita');
+
+select nombrecliente
+from clientes
+where nombrecliente like 'Jav\%as%';
+
+-- ando, ordenando, con un flow violento
+
+select *
+from clientes
+order by nombrecliente asc;
+
+-- Visualizar todos los atributos de tabla factura ordenados por id_cliente de manera descendente y la fecha de forma ascendente
+
+select * from factura
+order by factura_id_cliente_fk desc, fechafactura asc;
+
+-- Operaciones entre conjuntos
+
+(select distinct nombreart, colorart from detalle
+where nombreart = 'camisa')
+union
+(select nombreart, colorart from detalle
+where nombreart = 'jeans');
+
+(select distinct nombreart, colorart from detalle
+where nombreart = 'camisa')
+except
+(select nombreart, colorart from detalle
+where colorart = 'blanco');
+
+/* 
+Funciones de agregación
+(promedio: avg, mínimo: min, máximo: max, suma: sum, contador: count)
+*/
+
+select avg(valorpagado), id from pagos;
+
+select sum(valorpagado) as total from pagos;
+
+select count (distinct nombreart) from detalle;
+
+
+-- Agregación con agrupamiento
+
+select idpago, pagos_num_factura_fk, avg(valorpagado)
+from pagos
+group by idpago;
+
+select idpago, pagos_num_factura_fk, avg(valorpagado) from pagos
+group by idpago
+having avg(valorpagado) > 500001
