ca139f207cdf51b5a40e2e150ba4caaf0ecfec03
Simón Vélez <145301431+simonvelez@users.noreply.github.com>
2025-02-05 14:35:01 -0500

    Update client.py

--- Redes de Computadores/socket_basico_022025/client.py
+++ Redes de Computadores/socket_basico_022025/client.py
@@ -1,9 +1,10 @@
 import socket
 import struct
+import os
 
 def main():
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect(('localhost', 5555))
+    client.connect(('10.19.93.26', 5555))
 
 
     print("Connected to the server. Type messages to send.")
@@ -14,21 +15,22 @@ def main():
             filename = os.path.basename(file_path)
             file_size = os.path.getsize(file_path)
 
-            # Enviar el tamaño del nombre del archivo
+            # Enviar el tamanno del nombre del archivo
 
             filename_size = len(filename)
-            client.sendall(struc.pacl('!I'.filename.size))
+            client.sendall(struct.pack('!I',filename_size))
 
             # Enviar el nombre del archivo
 
-            client.sendall(struc.pack('!Q'.file:size))
+            client.sendall(struct.pack('!Q',file_size))
 
-            file = open(file_pack, 'rb')
+            file = open(file_path, 'rb')
             while True:
                 data = file.read(4096)
                 if not data: break
                 client.sendall(data)
-            print("File sent successfully")
+            print("File sent successfully :3")
+            file.close()
 
 
 if __name__ == "__main__":
