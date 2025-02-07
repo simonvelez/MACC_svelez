4741fc98598ac33925a192e47751c7735c1eea2f
Simón Vélez <145301431+simonvelez@users.noreply.github.com>
2025-02-07 14:48:36 -0500

    Enviar video

--- Redes de Computadores/socket_basico_022025/client.py
+++ Redes de Computadores/socket_basico_022025/client.py
@@ -1,36 +1,52 @@
 import socket
 import struct
+import cv2
+import numpy as np
 import os
 
 def main():
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect(('10.19.93.26', 5555))
 
 
     print("Connected to the server. Type messages to send.")
 
     while True:
-        file_path = input("Enter the path of the file to send:")
-        if os.path.isfile(file_path):
-            filename = os.path.basename(file_path)
-            file_size = os.path.getsize(file_path)
+        size = server.recv(16).decode('utf-8')
+        if not size:
+            break
+        data = b''
+        while len(data) < size:
+            packet = server.recv(size)
+            if not packet:
+                break
+            data += packet
+        frame_data = np.frombuffer(data, dtype= np.uint8)
+        frame = cv2.imdecode(frame_data, cv2.IMREAD_COLOR)
+        cv2.imshow('Video', frame)
+        cv2.waitkey(1)    
+    
+        # Código envío de archivos 050225
+        # file_path = input("Enter the path of the file to send:")
+        # if os.path.isfile(file_path):
+        #     filename = os.path.basename(file_path)
+        #     file_size = os.path.getsize(file_path)
 
-            # Enviar el tamanno del nombre del archivo
+        #     # Enviar el tamanno del nombre del archivo
 
-            filename_size = len(filename)
-            client.sendall(struct.pack('!I',filename_size))
+        #     filename_size = len(filename)
+        #     client.sendall(struct.pack('!I',filename_size))
 
-            # Enviar el nombre del archivo
+        #     # Enviar el nombre del archivo
 
-            client.sendall(struct.pack('!Q',file_size))
+        #     client.sendall(struct.pack('!Q',file_size))
 
-            file = open(file_path, 'rb')
-            while True:
-                data = file.read(4096)
-                if not data: break
-                client.sendall(data)
-            print("File sent successfully :3")
-            file.close()
+        #     file = open(file_path, 'rb')
+        #     while True:
+        #         data = file.read(4096)
+        #         if not data: break
+        #         client.sendall(data)
+        #     print("File sent successfully :3")
+        #     file.close()
 
 
 if __name__ == "__main__":
--- Redes de Computadores/socket_basico_022025/server.py
+++ Redes de Computadores/socket_basico_022025/server.py
@@ -1,57 +1,85 @@
 import socket
 import threading
-import struct
+import cv2
+import numpy as np
 
-def handle_client(client_socket):
+def handle_client(client_socket, videoCapture):
     while True:
         try:
-            # Recibir el tamaño del nombre del archivo
-            file_name_size = client_socket.recv(4)
-            if file_name_size:
-                filename_size = struct.unpack('!I', file_name_size)[0]
-
-            # Recibir el nombre del archivo
-            filename = client_socket.recv(filename_size).decode()
-            print("Receiving file:")
-            print(filename)
-
-            # Recibir el tamaño del archivo
-            file_size_data = client_socket.recv(8)
-            file_size = struct.unpack('!Q', file_size_data)[0]
-            print("File Size:")
-            print(file_size)
-
-            # Recibir el archivo por chunks
-            received_bytes = 0
-            file = open(filename, 'wb')
+        
+            ret, frame = videoCapture.read()
+            if not ret:
+                break;
+                
+            _, buffer = cv2.imencode('.jpg', frame)
+            data = np.array(buffer)
+            string_data = data.tostring()
             
-            while received_bytes < file_size:
-                chunk_size = 4096
-                if file_size - received_bytes < 4096:
-                    chunk_size = file_size - received_bytes
-                data = client_socket.recv(chunk_size)
-                if not data:
-                    break
-                file.write(data)
-                received_bytes += len(data)
+            client_socket.sendall(str(len(string_data))).ljust(16).encode('utf-8')
+            
+            client_socket.sendall(string_data)
+            
+       
+        # # Código para recibir y mandar archivos 050225
+        
+        #     # Recibir el tamaño del nombre del archivo
+        #     file_name_size = client_socket.recv(4)
+        #     if file_name_size:
+        #         filename_size = struct.unpack('!I', file_name_size)[0]
 
-            print("File received correctly")
+        #     # Recibir el nombre del archivo
+        #     filename = client_socket.recv(filename_size).decode()
+        #     print("Receiving file:")
+        #     print(filename)
 
+        #     # Recibir el tamaño del archivo
+        #     file_size_data = client_socket.recv(8)
+        #     file_size = struct.unpack('!Q', file_size_data)[0]
+        #     print("File Size:")
+        #     print(file_size)
+
+        #     # Recibir el archivo por chunks
+        #     received_bytes = 0
+        #     file = open(filename, 'wb')
+            
+        #     while received_bytes < file_size:
+        #         chunk_size = 4096
+        #         if file_size - received_bytes < 4096:
+        #             chunk_size = file_size - received_bytes
+        #         data = client_socket.recv(chunk_size)
+        #         if not data:
+        #             break
+        #         file.write(data)
+        #         received_bytes += len(data)
+
+        #     print("File received correctly")
+        
         except:
             print("Client disconnected")
             client_socket.close()
             break
 
+def show(videoCapture):
+    while True:
+        ret, frame = videoCapture.read()
+        cv2.imshow('Frame',frame)
+        cv2.waitKey(1)
+
 def main():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind(('localhost', 5555))
+    server.bind(('0.0.0.0', 5555))
     server.listen(5)
     print("Server started, waiting for connections...")
 
+    videoCapture = cv2.VideoCapture(0)
+    
+    show_camera = threading.Thread(target=show, args=(videoCapture,))
+    show_camera.start()
+    
     while True:
         client_socket, addr = server.accept()
         print(f"Connection from {addr} has been established!")
-        client_handler = threading.Thread(target=handle_client, args=(client_socket,))
+        client_handler = threading.Thread(target=handle_client, args=(videoCapture, client_socket,))
         client_handler.start()
 
 if __name__ == "__main__":
